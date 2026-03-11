805152becfe4d1c4bf6d816a2f05036b1368b4cf
oneminch <27751688+oneminch@users.noreply.github.com>
2026-03-11 17:14:06 -0400

    feat: setup worker with router utility

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { homeHandler, promoHandler, promosHandler } from "./handlers";
+import Router from "./router";
+
+const router = new Router()
+
+router
+	.get("/", homeHandler)
+	.get("/api/promo", promoHandler)
+	.get("/api/promos", promosHandler)
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		return router.fetch(request, env, ctx)
 	},
 } satisfies ExportedHandler<Env>;
+
