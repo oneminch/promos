5d9d073b9d7602039558b34b8b488d2c85cf14c8
oneminch <27751688+oneminch@users.noreply.github.com>
2026-03-11 17:11:56 -0400

    feat: create simple router utility

---
+++ src/router.ts
@@ -0,0 +1,58 @@
+import { defaultCatchAll } from "./handlers";
+import { ICallback, ISimpleRequest } from "./types";
+
+export default class Router {
+	routes: ISimpleRequest[];
+
+	constructor() {
+		this.routes = []
+
+		this.addCatchAllRoute()
+	}
+
+	public get(route: string, callback: ICallback) {
+		this.routes.push({
+			method: "GET",
+			route,
+			callback
+		})
+
+		return this
+	}
+
+	public post(route: string, callback: ICallback) {
+		this.routes.push({
+			method: "POST",
+			route,
+			callback
+		})
+
+		return this
+	}
+
+	public fetch(request: Request, env: Env, ctx: ExecutionContext): ReturnType<ICallback> {
+		const { pathname: requestPathname } = new URL(request.url)
+
+		const endpoint = this.routes.find((e) => (
+			e.method === request.method && e.route === requestPathname)
+		)
+
+		const catchAllEndpoint = this.routes.find((e) => (e.route === "*"))
+
+		if (!endpoint) {
+			return catchAllEndpoint!.callback(request, env, ctx)
+		}
+
+		return endpoint.callback(request, env, ctx)
+	}
+
+	public addCatchAllRoute(callback: ICallback = defaultCatchAll) {
+		this.routes.push({
+			method: "GET",
+			route: "*",
+			callback
+		})
+
+		return this
+	}
+}
